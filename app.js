245b0589f665a7004adb2193104168ea295a0dc4
Vlelouch <viswanathannova@outlook.com>
2021-08-18 11:45:20 +0530

    jsbot

---
+++ app.js
@@ -0,0 +1,55 @@
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf('1965130438:AAGkJt0f_WFAn3LN_N8X_xTvGNHsVPJinKk');
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+    })
+})
+
+bot.hears('phone', (ctx, next) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
+
+})
+
+//method for requesting user's location
+
+bot.hears("location", (ctx) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
+})
+
+//constructor for providing phone number to the bot
+
+const requestPhoneKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My phone number",
+                request_contact: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+};
+//constructor for proving location to the bot
+
+const requestLocationKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My location",
+                request_location: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+
+}
+
+bot.launch();
